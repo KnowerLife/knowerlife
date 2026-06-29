1d64a5633845fb55b54d9e686c75f320e05fc93e
KNOWER LIFE <knowerlife@yandex.ru>
2026-06-29 15:59:04 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'knowerlife-v1';
+const CACHE_NAME = 'knowerlife-v2';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -10,7 +10,7 @@ const urlsToCache = [
   '/icons/favicon-96x96.png',
   '/icons/apple-touch-icon.png',
   '/icons/web-app-manifest-192x192.png',
-  '/icons/web-app-manifest-512x512.png'
+  '/icons/web-app-manifest-512x512.png',
   'https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,400;14..32,500;14..32,600;14..32,700;14..32,800;14..32,900&family=JetBrains+Mono:wght@400;500;600&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
 ];
